c27b568762fb4cb1ff517864c03745a86f5dda5c
Голубенко М. И <golubenko.mi39@gmail.com>
2024-10-12 09:40:10 +0700

    Initial Commit

---
+++ ShkarovskyProjectApp/ShkarovskyProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShkarovskyProjectAppApp.swift
+//  ShkarovskyProjectApp
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShkarovskyProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
